a0288e19144c07773027342c00cccb0327b29164
davidobando99 <da.vi.d99@hotmail.com>
2020-07-14 16:42:20 -0500

    rama develop example

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <p></p>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
